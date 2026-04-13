2acc667927d7a4303faf01395f1b9311633cd15b
sheelestun <sheelestun@gmail.com>
2026-04-13 15:53:58 +0700

    added basic classes

---
+++ dev_alg/models.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+class Teacher:
+    def __init__(self, id: str, name: str, max_hours: int, available_days: List[str]):
+        self.id = id
+        self.name = name
+        self.max_hours = max_hours
+        self.available_days = available_days
+
+class Group:
+    def __init__(self, id: str, name: str, student_count: int):
+        self.id = id
+        self.name = name
+        self.student_count = student_count
+
+class Auditorium:
+    def __init__(self, id: str, capacity: int, type: str, available_days: List[str]):
+        self.id = id
+        self.capacity = capacity
+        self.type = type
+        self.available_days = available_days
+
+class Subject:
+    def __init__(self, id: str, name: str, hours_per_week: int, required_auditorium_type: str, is_lecture: bool):
+        self.id = id
+        self.name = name
+        self.hours_per_week = hours_per_week
+        self.required_auditorium_type = required_auditorium_type
+        self.is_lecture = is_lecture
+
+class LessonTask:
+    def __init__(self, id: str, teacher: Teacher, group: Group, subject: Subject):
+        self.id = id
+        self.teacher = teacher
+        self.group = group
+        self.subject = subject
